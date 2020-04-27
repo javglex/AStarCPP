c434c170f94e33d5ae62461aa222d5ca1ff19bc8
javier gonzalez <javglex@yahoo.com>
2020-04-26 21:13:51 -0700

    Initial Commit

---
+++ AStarCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AStarCPP
+//
+//  Created by Javier Gonzalez on 4/26/20.
+//  Copyright © 2020 Javier Gonzalez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
